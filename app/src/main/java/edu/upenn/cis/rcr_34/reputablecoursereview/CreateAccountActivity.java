3a1c11ccfb9573cca85c305d014b9890307a3b9a
Sagar Poudel <poudels14@gmail.com>
2015-03-29 15:57:14 -0400

    create account activity

--- app/src/main/java/edu/upenn/cis/rcr_34/reputablecoursereview/CreateAccountActivity.java
+++ app/src/main/java/edu/upenn/cis/rcr_34/reputablecoursereview/CreateAccountActivity.java
@@ -3,8 +3,18 @@ package edu.upenn.cis.rcr_34.reputablecoursereview;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.parse.FindCallback;
 import com.parse.Parse;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.SaveCallback;
+
+import java.util.List;
 
 /**
  * Created by sagar on 3/29/15.
@@ -22,5 +32,55 @@ public class CreateAccountActivity extends ActionBarActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         return true;
+    }
+
+    protected void registerClicked() {
+        EditText firstName = (EditText) findViewById(R.id.createaccount_firstname);
+        EditText lastName = (EditText) findViewById(R.id.createaccount_lastname);
+        EditText email = (EditText) findViewById(R.id.createaccount_email);
+        EditText year = (EditText) findViewById(R.id.createaccount_year);
+        EditText major = (EditText) findViewById(R.id.createaccount_major);
+        EditText password = (EditText) findViewById(R.id.createaccount_password);
+        EditText passwordConfirm = (EditText) findViewById(R.id.createaccount_passwordconfirmation);
+//        Button register = (Button) findViewById(R.id.createacccout_register_button);
+
+        // Check if password matches
+        if (!password.getText().equals(passwordConfirm.getText())) {
+            Toast.makeText(getApplicationContext(), "Password doesn't match!", Toast.LENGTH_SHORT).show();
+            password.setText("");
+            passwordConfirm.setText("");
+        } else {
+            createNewUser(firstName.getText().toString(), lastName.getText().toString(), email.getText().toString(), year.getText().toString(), password.getText().toString());
+        }
+    }
+
+    private void createNewUser(String firstName, String lastName, String email, String year, final String password) {
+        final ParseObject newObject = new ParseObject("userDetails");
+        newObject.put("firstName", firstName);
+        newObject.put("lastName", lastName);
+        newObject.put("email", email);
+        newObject.put("year", year);
+        newObject.put("password", password);
+
+        ParseQuery query = ParseQuery.getQuery("userDetails");
+        query.whereEqualTo("username", email);
+        query.findInBackground(new FindCallback<ParseObject>() {
+            @Override
+            public void done(List list, ParseException e) {
+                if (e == null) {
+                    if (list.size() > 0) {
+                        Toast.makeText(getApplicationContext(), "User Already Exists!", Toast.LENGTH_SHORT).show();
+                    } else {
+                        newObject.saveInBackground(new SaveCallback() {
+                            @Override
+                            public void done(ParseException e) {
+                                Toast.makeText(getApplicationContext(), "Account created", Toast.LENGTH_SHORT).show();
+//                                logIn();
+                            }
+                        });
+                    }
+                }
+            }
+        });
     }
 }
