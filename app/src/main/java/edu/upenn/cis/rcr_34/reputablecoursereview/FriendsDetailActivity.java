d9a1af914c0d5f241e49d7481cb17d2f4adc88f1
AndrewRemec <AndrewRemec@gmail.com>
2015-04-19 22:06:28 -0400

    Added person search

--- app/src/main/java/edu/upenn/cis/rcr_34/reputablecoursereview/FriendsDetailActivity.java
+++ app/src/main/java/edu/upenn/cis/rcr_34/reputablecoursereview/FriendsDetailActivity.java
@@ -13,7 +13,6 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
-
 import com.parse.ParseUser;
 
 import java.util.ArrayList;
